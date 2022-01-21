d555c03ed2de34787108929a20e605a3c3837273
ericksih <erick.saputra08@gmail.com>
2022-01-21 21:26:23 +0700

    Setup: icons, materialize, db.json, package.json

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+import React, { useEffect } from 'react';
 import './App.css';
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
 
 function App() {
+  useEffect(() => {
+    M.AutoInit(); // Initialize Materialize JS
+  });
+
   return (
     <div className='App'>
       <h1> Hello, World! </h1>
